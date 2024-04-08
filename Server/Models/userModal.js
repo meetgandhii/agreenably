163b04618d3aaaf5496a8df9b43a4d846c226d76
Meet Gandhi <meetgandhi@Meets-MacBook-Air.local>
2024-04-08 13:55:55 -0400

    user register website

--- Server/Models/userModal.js
+++ Server/Models/userModal.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   password: { type: String, required: true },
   email_address: { type: String, required: true },
+  website_address: { type: String, required: true },
   company_name : { type: String, required: true },
   interested_certifications: [
     { type: String, required: true },
